526893c80cafb0ae41ae6056956c60fa7e4342c2
Chirag Kantharia <chirag.kantharia@gmail.com>
2026-02-18 09:51:47 +0530

    feat(web): Update domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://chyrag.github.io',
-  base: '/cloudsre',
+  site: 'https://cloudsre.consulting',
+  base: '/',
 });
